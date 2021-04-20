eadd7e032a35416679fc787d3ce9ee4ec37d0092
zahwanugrah <39833642+zahwanugrah@users.noreply.github.com>
2021-04-20 20:21:53 +0800

    Update ins-vt.sh

--- ins-vt.sh
+++ ins-vt.sh
@@ -13,7 +13,7 @@ chronyc sourcestats -v
 chronyc tracking -v
 date
 # install v2ray
-wget https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/go.sh && chmod +x go.sh && ./go.sh
+wget https://raw.githubusercontent.com/zahwanugrah/AutoScriptSSH/main/go.sh && chmod +x go.sh && ./go.sh
 rm -f /root/go.sh
 mkdir /root/.acme.sh
 curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
@@ -506,22 +506,22 @@ systemctl start v2ray@trojan.service
 systemctl restart v2ray
 systemctl enable v2ray
 cd /usr/bin
-wget -O addws "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/addws.sh"
-wget -O addvless "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/addvless.sh"
-wget -O addtr "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/addtr.sh"
-wget -O delws "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/delws.sh"
-wget -O delvless "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/delvless.sh"
-wget -O deltr "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/deltr.sh"
-wget -O cekws "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/cekws.sh"
-wget -O cekvless "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/cekvless.sh"
-wget -O cektr "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/cektr.sh"
-wget -O renewws "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/renewws.sh"
-wget -O renewvless "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/renewvless.sh"
-wget -O renewtr "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/renewtr.sh"
-wget -O xp-ws "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/xp-ws.sh"
-wget -O xp-tr "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/xp-tr.sh"
-wget -O xp-vless "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/xp-vless.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/cert.sh"
+wget -O addws "https://raw.githubusercontent.com/zahwanugrah/AutoScriptSSH/main/addws.sh"
+wget -O addvless "https://raw.githubusercontent.com/zahwanugrah/AutoScriptSSH/main/addvless.sh"
+wget -O addtr "https://raw.githubusercontent.com/zahwanugrah/AutoScriptSSH/main/addtr.sh"
+wget -O delws "https://raw.githubusercontent.com/zahwanugrah/AutoScriptSSH/main/delws.sh"
+wget -O delvless "https://raw.githubusercontent.com/zahwanugrah/AutoScriptSSH/main/delvless.sh"
+wget -O deltr "https://raw.githubusercontent.com/zahwanugrah/AutoScriptSSH/main/deltr.sh"
+wget -O cekws "https://raw.githubusercontent.com/zahwanugrah/AutoScriptSSH/main/cekws.sh"
+wget -O cekvless "https://raw.githubusercontent.com/zahwanugrah/AutoScriptSSH/main/cekvless.sh"
+wget -O cektr "https://raw.githubusercontent.com/zahwanugrah/AutoScriptSSH/main/cektr.sh"
+wget -O renewws "https://raw.githubusercontent.com/zahwanugrah/AutoScriptSSH/main/renewws.sh"
+wget -O renewvless "https://raw.githubusercontent.com/zahwanugrah/AutoScriptSSH/main/renewvless.sh"
+wget -O renewtr "https://raw.githubusercontent.com/zahwanugrah/AutoScriptSSH/main/renewtr.sh"
+wget -O xp-ws "https://raw.githubusercontent.com/zahwanugrah/AutoScriptSSH/main/xp-ws.sh"
+wget -O xp-tr "https://raw.githubusercontent.com/zahwanugrah/AutoScriptSSH/main/xp-tr.sh"
+wget -O xp-vless "https://raw.githubusercontent.com/zahwanugrah/AutoScriptSSH/main/xp-vless.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/zahwanugrah/AutoScriptSSH/main/cert.sh"
 chmod +x addws
 chmod +x addvless
 chmod +x addtr
